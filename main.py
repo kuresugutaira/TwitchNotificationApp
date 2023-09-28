ddc7cb89322d6fefd3ee4a7a96616559f915c801
kuresugutaira <kuresugutaira@gmail.com>
2023-09-28 12:03:06 +0900

    utilsにgetAccessToken関数を抽出

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import functions_framework
 from flask import jsonify, Request, make_response
 import hmac
 import hashlib
-from utils.utils import getEnvVars
+from utils.utils import getEnvVars, getAccessToken
 from dotenv import load_dotenv
 import requests
 import json
@@ -45,38 +45,6 @@ def isValidSignature(request: Request, secret: str) -> bool:
     header_signature = headers["Twitch-Eventsub-Message-Signature"]
     # 比較結果を返す
     return hmac.compare_digest(expected_signature, header_signature)
-
-
-def getAccessToken(twitch_client_id: str, twitch_client_secret: str, get_access_token_uri: str) -> str:
-    ''' 
-    TwitchのAPIを叩くのに必要なトークンを取得する関数
-
-    -----
-    Args:
-        ``twitch_client_id`` (str) : Twitch DevelopersのClientID\n
-        ``twitch_client_secret`` (str) : Twitch DevelopersのClientSecret\n
-        ``get_access_token_uri`` (str) : トークンを得るために叩くAPIのURI
-    Returns:
-        ``access_token`` (str) : トークン
-    '''
-
-    request_body: dict[str, str] = {
-        'client_id': twitch_client_id,
-        'client_secret': twitch_client_secret,
-        'grant_type': 'client_credentials'
-    }
-    request_header: dict[str, str] = {
-        'content-type': 'application/x-www-form-urlencoded'
-    }
-    print("Twitch APIのApp Access Tokenを取得します")
-    response: requests.Response = requests.post(
-        get_access_token_uri, headers=request_header, data=request_body)
-    access_token: str = (response.json())["access_token"]
-    if access_token is not None:
-        print("App Access Tokenの取得に成功しました")
-    else:
-        print("App Access Tokenの取得に失敗しました")
-    return access_token
 
 
 def notifyToDiscord(data: NotificationData, discord_webhook_url: str, discord_icon_url: str) -> requests.Response:
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,7 @@
 import os
+import requests
 from typing import Optional
+
 
 def getEnvVars(key: str) -> str:
     '''
@@ -12,3 +14,35 @@ def getEnvVars(key: str) -> str:
     else:
         print(key + "の値が取得できませんでした")
         exit(1)
+
+
+def getAccessToken(twitch_client_id: str, twitch_client_secret: str, get_access_token_uri: str) -> str:
+    ''' 
+    TwitchのAPIを叩くのに必要なトークンを取得する関数
+
+    -----
+    Args:
+        ``twitch_client_id`` (str) : Twitch DevelopersのClientID\n
+        ``twitch_client_secret`` (str) : Twitch DevelopersのClientSecret\n
+        ``get_access_token_uri`` (str) : トークンを得るために叩くAPIのURI
+    Returns:
+        ``access_token`` (str) : トークン
+    '''
+
+    request_body: dict[str, str] = {
+        'client_id': twitch_client_id,
+        'client_secret': twitch_client_secret,
+        'grant_type': 'client_credentials'
+    }
+    request_header: dict[str, str] = {
+        'content-type': 'application/x-www-form-urlencoded'
+    }
+    print("Twitch APIのApp Access Tokenを取得します")
+    response: requests.Response = requests.post(
+        get_access_token_uri, headers=request_header, data=request_body)
+    access_token: str = (response.json())["access_token"]
+    if access_token is not None:
+        print("App Access Tokenの取得に成功しました")
+    else:
+        print("App Access Tokenの取得に失敗しました")
+    return access_token
